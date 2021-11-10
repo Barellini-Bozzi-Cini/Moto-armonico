c65deaf9acc14d8c209247271325e14d8080312c
Theramar <lorenzo1.cini@gmail.com>
2021-11-10 22:18:21 +0100

    inizio 3

--- main.cpp
+++ main.cpp
@@ -1,31 +1,49 @@
-#include <iostream>
 #include <cstdint>
+#include <array>
+#include <exception>
+
 using namespace std;
 
-int main (){
-int64_t scelta;
-
-cout <<"Scegli un'opzione:"
-<<"aggiungere varie opzioni\n";
-
-
-cin >>scelta;
 // s = A cos(wt)
 // v = -Aw sin(wt)
 // a = -Aw^2 cos(wt)
 // T = 1/f
 // w = 2pigreco/T
-switch (scelta) {
-case 1:
-	break;
 
-case 2:
-	break;
+constexpr double pi = 3.141592654;
+constexpr int lenght = 7; //numero di variabili int64_t
 
-default:
+using arr = std::array<bool, lenght>;
 
-	cout<<"\nUscita";
+enum variabili{
+	SPAZIO,
+	ACEELERAZIONE,
+	PULSAZIONE,
+	FREQUENZA,
+	TEMPO,
+	AMPIEZZA,
+	VELOCITA
+};
+
+using error = std::exception;
+
+auto find0(arr a){
+	for (auto& i : a) {
+		if (i == 0) return i;
+	}
+	throw "Nessuna variabile da calcolare";
 }
+
+auto spazio(arr a) {
+
+}
+
+int main (){
+int64_t spazio, accelerazione, pulsazione, frequenza, tempo, ampiezza, velocità; 
+//input da ui di Android?
+arr varInserita = { 0 }; 
+//array che ricorda se un valore è inserito da utente o meno
+
 
 return 0;
 }
