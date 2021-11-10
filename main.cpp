e4515009fbeb09349053921e951ac9600fdcabd9
JunjoSick <78446819+JunjoSick@users.noreply.github.com>
2021-11-10 21:26:00 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
-int main() {
-	//menu
-	switch () {
+#include <iostream>
+using namespace std;
 
-	}
+int main (){
+int scelta;
+
+cout<<"Scegli un'opzione:"
+<<"aggiungere varie opzioni"<<
+<<endl;
+
+
+cin>>scelta;
+
+switch (scelta) {
+case 1:
+	break;
+
+case 2:
+	break;
+
+default:
+
+	cout<<"\nUscita";
+}
+
+return 0;
 }
