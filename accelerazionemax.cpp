6a315eab170dfe8a1037f45544e3cdecdae2026b
JunjoSick <78446819+JunjoSick@users.noreply.github.com>
2021-11-10 23:06:28 +0100

    Add files via upload
    revisionare

---
+++ accelerazionemax.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main (){
+double amp, puls;
+cout<<"\n**Calcolo dell'accelerazione massima**"
+<<"\nInserire ampiezza in **: "; //unità di misura
+cin>>amp;
+cout<<"\nInserire pulsazione in **: "; //unità di misura
+cin>>puls;
+cout<<"\nAccellerazione massima: "<< amp*puls*puls << "**"; //unitaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+//ritorno alla schermata precedente
+}
