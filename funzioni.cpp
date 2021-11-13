b2b5c6d92cbd4191adfe725204129f9940fd7e59
Lorenzo <lorenzo1.cini@gmail.com>
2021-11-13 21:55:51 +0100

    funzione.cpp 2

--- funzioni.cpp
+++ funzioni.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include "equation_solver.cpp"
 auto posizione() {
 	return A * cos(w * t);
 	return a / -pow(w, 2);
@@ -7,27 +8,29 @@ auto tempo() {
 	return acos(x / a) / w;
 	return asin(v/(-A*w))/w
 }
-auto tempo() {
+auto accelerazione() {
+	return -A * pow(w, 2) * cos(w * t);
+	return -x * pow(w, 2);
+}
+auto periodo() {
+	return 1 / f;
+	return 2 * pi / T;
+}
+auto velocit‡() {
+	return -A * w * sin(w * t);
+}
+auto velocit‡Max() {
+	return A * w;
+}
+auto accelerazioneMax() {
+	return A * pow(w, 2);
+}
+auto pulsazione() {
+	return acos(x / A) / t;
+}
+auto frequenza() {
 	out <<
 }
-auto tempo() {
-	out <<
-}
-auto tempo() {
-	out <<
-}
-auto tempo() {
-	out <<
-}
-auto tempo() {
-	out <<
-}
-auto tempo() {
-	out <<
-}
-auto tempo() {
-	out <<
-}
-auto tempo() {
+auto ampiezza() {
 	out <<
 }
