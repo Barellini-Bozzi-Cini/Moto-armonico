b1c28f408c5fd084a712cfe4d74a4d64f132ac01
Lorenzo <lorenzo1.cini@gmail.com>
2021-11-13 21:47:05 +0100

    file funzioni 1

---
+++ funzioni.cpp
@@ -0,0 +1,33 @@
+#include <cmath>
+auto posizione() {
+	return A * cos(w * t);
+	return a / -pow(w, 2);
+}
+auto tempo() {
+	return acos(x / a) / w;
+	return asin(v/(-A*w))/w
+}
+auto tempo() {
+	out <<
+}
+auto tempo() {
+	out <<
+}
+auto tempo() {
+	out <<
+}
+auto tempo() {
+	out <<
+}
+auto tempo() {
+	out <<
+}
+auto tempo() {
+	out <<
+}
+auto tempo() {
+	out <<
+}
+auto tempo() {
+	out <<
+}
